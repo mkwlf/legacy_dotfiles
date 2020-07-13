9d2b3e58ba68ca49e6d3c85b62f342ea8fe7dc21
Michael Wolf <mike.wolf@antiloop.com>
2020-07-13 13:33:37 +0200

    [zsh] back to agnoster oh-my-zsh theme

--- zsh/theme.zsh
+++
@@ -1,5 +0,0 @@
-# ZSH_THEME="avit"
-# POWERLEVEL9K_MODE="nerdfont-complete"
-# POWERLEVEL9K_DISABLE_RPROMPT=true
-# POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(virtualenv dir vcs)
-source /usr/local/opt/powerlevel10k/powerlevel10k.zsh-theme
