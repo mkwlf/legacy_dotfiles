3735065309b5e5e198dbd7630a1dcf9eb9f08332
Michael Wolf <mike.wolf@antiloop.com>
2020-07-14 14:14:55 +0200

    [zsh] adds spryker docker/sdk aliases

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,5 +1,11 @@
 alias ping-google="ping 8.8.8.8 -c 8"
 alias ll="lsd -l"
 alias la="lsd -la"
-alias prp="pipenv run python"
-alias sprkr="APPLICATION_ENV=development vendor/bin/console"
+alias prp="pipenv run python"#
+
+# spryker
+alias spryker="docker/sdk console"
+alias spryk="spryker spryk:run"
+alias spryk-build="spryker spryk:build"
+alias spryk-list="spryker spryk:dump"
+
